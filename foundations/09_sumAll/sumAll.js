581b31a758c9bb5b3da66441a9ae973174c8a21b
ni-lie <williejr@twelvebaskets.com.ph>
2026-01-29 13:54:44 +0800

    feat: complete exer 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof start !== 'number' || typeof end !== 'number' || !Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) return 'ERROR';
+    let arrayOfIntegers = []; 
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    for (let i=start; i<=end; i++) {
+        arrayOfIntegers.push(i);
+    } 
+    return sum = arrayOfIntegers.reduce((total, item) => total + item, 0)
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
