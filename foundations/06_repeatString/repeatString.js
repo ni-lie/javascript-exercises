0485b7ef89c116a1564058603880294cc41b72cd
ni-lie <williejr@twelvebaskets.com.ph>
2026-01-29 12:03:48 +0800

    feat: complete exercise 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(inputString, num) {
+    let outputString = "";
+    if (num < 0) return "ERROR";
 
+    for (let i = 0; i < num; i++) {
+        outputString += inputString;
+    }
+    return outputString
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
