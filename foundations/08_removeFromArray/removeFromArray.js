d84b989cdae73a43bc5f7186421e114d14370b51
ni-lie <williejr@twelvebaskets.com.ph>
2026-01-29 13:36:51 +0800

    feat: do exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...arrayOfValuesToRemove) {
+    filteredArray = inputArray.filter(item => !arrayOfValuesToRemove.includes(item));
+    return filteredArray;
 };
 
 // Do not edit below this line
