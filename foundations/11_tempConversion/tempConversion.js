7bc69cdbb8ad9d21e6828dcfac07c948c02372b7
ni-lie <williejr@twelvebaskets.com.ph>
2026-01-30 09:41:30 +0800

    feat: finish exerr 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsius = (fahrenheit - 32) * (5 / 9);
+  celsiusRoundedToOneDecimal = Math.round(celsius * 10) / 10;
+  return celsiusRoundedToOneDecimal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = (celsius * (9 / 5)) + 32;
+  fahrenheitRoundedToOneDecimal = Math.round(fahrenheit * 10) / 10;
+  return fahrenheitRoundedToOneDecimal;
 };
 
 // Do not edit below this line
