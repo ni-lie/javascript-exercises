efb5b2f0ed1a1162ae99837d497d2baed33ccb11
ni-lie <williejr@twelvebaskets.com.ph>
2026-01-30 09:34:48 +0800

    feat: finish exer 10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    if ((inputYear % 4 === 0 && inputYear % 100 !== 0) || (inputYear % 400 === 0))
+        return true;
+    else
+        return false;
 };
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
